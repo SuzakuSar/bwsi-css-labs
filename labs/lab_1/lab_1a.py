5b84d1d815d76b38c20bc7d979b0cd2d89c7229d
SuzakuSar <loganjocsonsar@gmail.com>
2026-03-28 13:21:44 -0700

    added intro

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,7 @@ def main():
     name = "Logan Sar" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
+    print("Hello, Logan Sar here. I'm a junior currently at Cosumnes Oaks (California) and I'm a tad late to the game. I just got the email to verify and now I'm sort of racing to complete the courses before the 30th. My story is that I just started looking for courses and ways to learn and I stumbled upon this and decided that I have enough time to finish it in 5 days. Many tips would be appreciate especially those surrounding the required amount of curriculum I have to complete by the 30th. Cheers! Oh yeah and fun fact(s) I was varsity on wrestling during freshman year but the year after my parents made me quit for FRC robotics (Wolfpack robotics 3859) where I was co-driver/programmer just for the team to disband that same year. I am now on Eagleforce (Pleasant Grove High School's team) as a programmer.")
 
 if __name__ == "__main__":
     main()
